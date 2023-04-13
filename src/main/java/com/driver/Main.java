63f992d28f223771b4a99d5eebc896872c608f9a
Shubham Jauhari <90859295+Jauhari-01@users.noreply.github.com>
2023-04-13 14:40:21 +0000

    Correction done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,15 +3,12 @@ package com.driver;
 public class Main {
     public static class Product{
         public int product(int x,int y){
-            System.out.println("HelloWorld");
             return x+y;
         }
         public int product(int x,int y,int z){
-            System.out.println("HelloWorld2");
             return x+y+z;
         }
         public double product(double x,double y){
-            System.out.println("HelloWorld3");
             return x+y;
         }
     }
